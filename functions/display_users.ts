7133ef6206c49aa3ae5cfd148c9efc77961d14b4
Joshua Aresty <josh.aresty@movehq.com>
2023-06-20 13:29:12 -0700

    reference user IDs when posting in thread

---
+++ functions/display_users.ts
@@ -0,0 +1,40 @@
+import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
+
+export const DisplayUsersDefinition = DefineFunction({
+    callback_id: "display_users",
+    title: "Display Users",
+    description: "display an array of user ids",
+    source_file: "functions/display_users.ts",
+    input_parameters: {
+        properties: {
+            users: {
+                type: Schema.types.array,
+                items: {
+                    type: Schema.slack.types.user_id,
+                },
+                description: "the users to display",
+            }
+        },
+        required: ["users"],
+    },
+    output_parameters: {
+        properties: {
+            display_users: {
+                type: Schema.types.string,
+                description: "A string of the user IDs to display",
+            },
+        },
+        required: ["joined_users"],
+    }
+})
+
+export default SlackFunction(
+    DisplayUsersDefinition,
+    ({ inputs }) => {
+        if(inputs.users.length == 0) {
+            return { outputs: { joined_users: [] }};
+        }
+        const display_users = inputs.users.map((user_id) => `<@${user_id}>`).join(',');
+        return { outputs: { display_users }};
+    },
+);
